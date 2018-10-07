e6cd8a9f2c672b9c0a5be78fed5e1157e343d669
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2018-10-07 16:32:12 +0200

    Instalación de módulos: 7Zip4Powershell y WinSCP
    Set-ExecutionPolicy, 7Zip4Powershell y WinSCP

--- instalar-modulos.ps1
+++ instalar-modulos.ps1
@@ -2,9 +2,9 @@
 # Autor @adrianlois_ #
 ######################
 
-# Quitar restricciones de ejecución de scripts
+# Quitar restricciones de ejecuciÃ³n de scripts
 Set-ExecutionPolicy Unrestricted -scope LocalMachine -Force
 
-# Instalación de módulos 7Zip4Powershell y WinSCP.
+# InstalaciÃ³n de mÃ³dulos 7Zip4Powershell y WinSCP
 Install-Module -Name 7Zip4Powershell -Verbose -Force
 Install-Module -Name WinSCP -Verbose -Force
