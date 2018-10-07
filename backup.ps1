32a5a45f8b7e702e2a5eb347388b080386775eed
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2018-10-07 15:10:43 +0200

    Automatizar Backups FTP PowerShell

--- backup.ps1
+++ backup.ps1
@@ -8,7 +8,7 @@
 # Quitar restricciones de ejecución de scripts
 # Set-ExecutionPolicy Unrestricted -scope LocalMachine -Force
 
-# Instalación de módulos 7Zip4Powershell y WinSCP.
+# Instalación de módulos 7Zip4Powershell y WinSCP
 # Install-Module -Name 7Zip4Powershell -Verbose -Force
 # Install-Module -Name WinSCP -Verbose -Force
 ## Fin Prerequisitos ##
